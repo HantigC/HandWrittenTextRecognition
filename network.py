14b4682e3aa7c33939d3d08fa6e6eee783e67741
HantigC <cristianhantig96@gmail.com>
2018-12-05 19:26:14 +0100

    started neural network impl

--- network.py
+++ network.py
@@ -20,6 +20,20 @@ class NeuralNetwork:
         for b, w in zip(self.biases, self.weights):
             x = sigmoid(np.dot(w, x) + b)
 
-    def back_propagation(self):
-        pass
+    def back_propagation(self, x, y):
+        raise NotImplementedError("Implement back propagation method")
 
+    def evaluate(self, test_data):
+        test_result = [(self.feed_forward(x), y) for (x, y) in test_data]
+        _test_result = sum(int(x == y) for (x, y) in test_result)
+        return float(_test_result) / float(len(test_result))
+
+    def learn(self, batch, learning_rate):
+        delta_b = [np.zeros(b.shape) for b in self.biases]
+        delta_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in batch:
+            grad_b, grad_w = self.back_propagation(x, y)
+            delta_b = [b + db for b, db in zip(grad_b, delta_b)]
+            delta_w = [w + dw for w, dw in zip(grad_w, delta_w)]
+        self.weights = [w - (learning_rate / len(batch)) * dw for w, dw in zip(self.weights, delta_w)]
+        self.biases = [b - (learning_rate) / len(batch) * db for b, db in zip(self.biases, delta_b)]
